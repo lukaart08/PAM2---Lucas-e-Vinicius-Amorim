0d44fbc12845eea0be04058c80db70a0a376775c
lukaart08 <lucascdrive@gmail.com>
2026-06-15 10:44:04 +0000

    Busca busca jogador, Vers 1.0

--- reactvazio/vazio/App.js
+++ reactvazio/vazio/App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
   return (
+    <ScrollView>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Coloque seu nome</Text>
       <StatusBar style="auto" />
     </View>
+    <TextInput
+      style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="Nome"
+    />
+    <Image
+          source={{
+            uri: 'https://media.tenor.com/RKDWKVSUihYAAAAj/suspicious-emoji.gif',
+          }}
+          style={{width: 200, height: 200}}
+        />
+    </ScrollView>
   );
 }
 
